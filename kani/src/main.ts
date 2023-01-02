ee3ea3742388e3a4720c7ba1a4357d2d50559392
kani <xenoklikz@gmail.com>
2023-01-01 18:49:37 -0600

    Created titles for each component

--- kani/src/main.ts
+++ kani/src/main.ts
@@ -5,6 +5,7 @@ import Projects from "./pages/Projects/MyProjects.vue";
 import Lessons from "./pages/Lessons/LessonsPage.vue";
 import Books from "./pages/Books/BookList.vue";
 import App from "./App.vue";
+const DEFAULT_TITLE = "QK";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,23 +14,39 @@ const router = createRouter({
       path: "/",
       name: "journey",
       component: Journey,
+      meta: {
+        title: "Home",
+      },
     },
     {
       path: "/projects",
       name: "projects",
       component: Projects,
+      meta: {
+        title: "Projects",
+      },
     },
     {
       path: "/lessons",
       name: "lessons",
       component: Lessons,
+      meta: {
+        title: "Lessons",
+      },
     },
     {
       path: "/books",
       name: "books",
       component: Books,
+      meta: {
+        title: "Books",
+      },
     },
   ],
+});
+
+router.afterEach((to) => {
+  document.title = (to.meta.title as string) || DEFAULT_TITLE;
 });
 
 createApp(App).use(router).mount("#app");
