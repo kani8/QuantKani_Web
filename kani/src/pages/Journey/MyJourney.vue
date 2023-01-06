f365bfa30c27589f8a63d0eb5178e79378999c94
kani <xenoklikz@gmail.com>
2023-01-05 20:53:17 -0600

    Desktop for Quasar Framework 90%

--- kani/src/pages/Journey/MyJourney.vue
+++ kani/src/pages/Journey/MyJourney.vue
@@ -1,28 +1,77 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/"
-        ><h2 class="logo">Quant<span>Kani</span></h2></router-link
-      >
-      <ul>
-        <li><router-link to="/projects">Projects</router-link></li>
-        <li><router-link to="/books">Books</router-link></li>
-        <li><router-link to="/lessons">Lessons</router-link></li>
-      </ul>
-      <button
-        class="mode"
-        aria-label="Switch Theme"
-        @click="applyTheme"
-      ></button>
-    </nav>
-
-    <section class="kani" ref="kani">
-      <div class="console-container">
-        <span id="text"></span>
-        <div class="console-underscore" id="console">&#95;</div>
-      </div>
-    </section>
-  </div>
+  <q-layout view="lHh Lpr fFf" class="q-ma-md" style="height: 100%">
+    <q-header elevated class="bg-black text-white shadow-2">
+      <q-toolbar>
+        <q-toolbar-title class="heading"
+          >Quant<span>Kani</span></q-toolbar-title
+        >
+        <q-btn
+          flat
+          round
+          dense
+          class="q-ma-xs"
+          @click="drawer = !drawer"
+          aria-label="Toggle Sidebar"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+      </q-toolbar>
+      <q-tabs v-model="tab" class="pages">
+        <router-link to="/projects"
+          ><q-tab name="Projects" label="Projects"></q-tab
+        ></router-link>
+        <router-link to="/books"
+          ><q-tab name="Books" label="Books"></q-tab
+        ></router-link>
+        <router-link to="/lessons"
+          ><q-tab name="Lessons" label="Lessons"></q-tab
+        ></router-link>
+      </q-tabs>
+    </q-header>
+    <q-drawer
+      v-model="drawer"
+      side="right"
+      bordered
+      :width="250"
+      class="bg-black text-white"
+      hide-on-escape
+    >
+      <q-list link>
+        <q-item>
+          <q-item-side>
+            <q-icon name="home" color="white" />
+          </q-item-side>
+          <q-item-main label="Home" />
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-icon name="folder" color="white" />
+          </q-item-side>
+          <q-item-main label="Projects" />
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-icon name="book" color="white" />
+          </q-item-side>
+          <q-item-main label="Books" />
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-icon name="school" color="white" />
+          </q-item-side>
+          <q-item-main label="Lessons" />
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-page-container class="q-pa-md q-ma-md">
+      <section class="kani" ref="kani">
+        <div class="console-container">
+          <span id="text"></span>
+          <div class="console-underscore" id="console">&#95;</div>
+        </div>
+      </section>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script lang="ts">
@@ -31,6 +80,12 @@ import { consoleText } from "../../assets/ts/script";
 import state, { setTheme, toggleTheme } from "../../assets/ts/theme";
 
 export default defineComponent({
+  data() {
+    return {
+      drawer: false,
+      tab: 0,
+    };
+  },
   mounted() {
     if (!state.theme) {
       setTheme("dark");
--- kani/src/pages/Lessons/LessonsPage.vue
+++ kani/src/pages/Lessons/LessonsPage.vue
@@ -1,19 +1,70 @@
 <template>
-  <div id="app">
-    <div>
-      <nav>
-        <router-link to="/"
-          ><h2 class="logo">Quant<span>Kani</span></h2></router-link
+  <q-layout view="lHh Lpr fFf" class="q-ma-md" style="height: 100%">
+    <q-header elevated class="bg-black text-white shadow-2">
+      <q-toolbar>
+        <q-toolbar-title class="heading"
+          >Quant<span>Kani</span></q-toolbar-title
         >
-        <ul>
-          <li><router-link to="/projects">Projects</router-link></li>
-          <li><router-link to="/books">Books</router-link></li>
-          <li><router-link to="/lessons">Lessons</router-link></li>
-        </ul>
-        <button class="mode" @click="applyTheme"></button>
-      </nav>
-    </div>
-  </div>
+        <q-btn
+          flat
+          round
+          dense
+          class="q-ma-xs"
+          @click="drawer = !drawer"
+          aria-label="Toggle Sidebar"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+      </q-toolbar>
+      <q-tabs v-model="tab" class="pages">
+        <router-link to="/projects"
+          ><q-tab name="Projects" label="Projects"></q-tab
+        ></router-link>
+        <router-link to="/books"
+          ><q-tab name="Books" label="Books"></q-tab
+        ></router-link>
+        <router-link to="/lessons"
+          ><q-tab name="Lessons" label="Lessons"></q-tab
+        ></router-link>
+      </q-tabs>
+    </q-header>
+    <q-drawer
+      v-model="drawer"
+      side="right"
+      bordered
+      :width="250"
+      class="bg-black text-white"
+      hide-on-escape
+    >
+      <q-list link>
+        <q-item>
+          <q-item-side>
+            <q-icon name="home" color="white" />
+          </q-item-side>
+          <q-item-main label="Home" />
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-icon name="folder" color="white" />
+          </q-item-side>
+          <q-item-main label="Projects" />
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-icon name="book" color="white" />
+          </q-item-side>
+          <q-item-main label="Books" />
+        </q-item>
+        <q-item>
+          <q-item-side>
+            <q-icon name="school" color="white" />
+          </q-item-side>
+          <q-item-main label="Lessons" />
+        </q-item>
+      </q-list>
+    </q-drawer>
+    <q-page-container class="q-pa-md q-ma-md"> </q-page-container>
+  </q-layout>
 </template>
 
 <script lang="ts">
@@ -23,13 +74,13 @@ import { toggleTheme } from "../../assets/ts/theme";
 export default defineComponent({
   data() {
     return {
-      tracker: 0,
+      drawer: false,
+      tab: 0,
     };
   },
   methods: {
     applyTheme() {
       toggleTheme();
-      this.tracker++;
     },
   },
 });
