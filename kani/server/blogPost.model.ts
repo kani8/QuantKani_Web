860d832f29388776e2ce09b3a054c3c853ff0334
kani <xenoklikz@gmail.com>
2023-07-12 17:36:38 -0500

    Fixes

--- kani/server/blogPost.model.ts
+++ kani/server/blogPost.model.ts
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const blogPostSchema = new mongoose.Schema({
   title: String,
   content: String,
-  // Add any other properties you need
 });
 
 const BlogPost = mongoose.model("BlogPost", blogPostSchema);
